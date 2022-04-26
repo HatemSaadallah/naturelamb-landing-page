9b633ead543f51d9c85df8e534a9d0eb2ffe8e3a
Hatem Saadallah <saadallah.r.hatem@gmail.com>
2022-04-27 02:49:13 +0300

    Created Header Part

--- src/Components/Header/header_part.component.js
+++ src/Components/Header/header_part.component.js
@@ -1,7 +1,6 @@
-export default function Header() {
+export default function HeaderPart() {
     return (
         <div className="header">
-
             <h2>Header</h2>
         </div>
     );
--- src/Views/Home/home.view.js
+++ src/Views/Home/home.view.js
@@ -1,10 +1,47 @@
-import Navbar from "../../Components/Navbar/navbar.componenet";
+import Navbar from "../../Components/Navbar/navbar.component";
+import { Layout } from 'antd';
+import 'antd/dist/antd.css';
+import HeaderPart from "../../Components/Header/header_part.component";
+const { Header, Content, Footer } = Layout;
 
 export default function Home() {
     return (
         <div>
-            <Navbar />
-            <Header />
+            <Layout>
+                <Header
+                    style={{
+                        zIndex: 1,
+                        width: '100%',
+                    }}
+                >
+                    <div className="logo" />
+                    <Navbar />
+                </Header>
+                <Content
+                    className="site-layout"
+                    style={{
+                        padding: '0 50px',
+                        marginTop: 64,
+                    }}
+                >
+                    <div
+                        className="site-layout-background"
+                        style={{
+                            padding: 24,
+                            minHeight: 380,
+                        }}
+                    >
+                        <HeaderPart />
+                        Content
+                    </div>
+                </Content>
+                <Footer
+                    style={{
+                        textAlign: 'center',
+                    }}
+                >
+                </Footer>
+            </Layout>
         </div>
     );
 }
