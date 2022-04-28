297da905ae82fe6555af728ed4b1cd9e4fb0ec3d
Hatem Saadallah <saadallah.r.hatem@gmail.com>
2022-04-28 22:39:11 +0300

    Implemented Cards View

--- src/Views/Home/home.view.js
+++ src/Views/Home/home.view.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import Navb from "../../Components/Navbar/navbar";
-import SmallCard from "../../Components/Card/card";
-import Navbar from "../../Components/Navbar/navbar.component";
 import { Layout } from 'antd';
 import 'antd/dist/antd.css';
 import HeaderPart from "../../Components/Header/header_part.component";
-const { Header, Content, Footer } = Layout;
+import CardsView from '../../Components/CardsView/cards.view';
+const { Content, Footer } = Layout;
 
 export default function Home() {
     return (
         <div>
-            <Layout>
+            {/* Change layout background color */}
+            <Layout style={{ backgroundColor: '#fff' }}>
                 <Navb />
                 <Content
                     className="site-layout"
@@ -19,20 +19,18 @@ export default function Home() {
                         marginTop: 64,
                     }}
                 >
-                    <div
-                        className="site-layout-background"
-                        style={{
-                            padding: 24,
-                            minHeight: 380,
-                        }}
-                    >
-                        <HeaderPart />
-                        Content
+                    {/* <div className="site-layout-background"> */}
+                    <HeaderPart />
+                    {/* </div> */}
+                    <br /> <br /> <br />
+                    <div className='cards-view'>
+                        <CardsView />
                     </div>
                 </Content>
                 <Footer
                     style={{
                         textAlign: 'center',
+                        backgroundColor: '#fff',
                     }}
                 >
                 </Footer>
@@ -40,24 +38,3 @@ export default function Home() {
         </div>
     );
 }
-
-// import React from 'react';
-// import Navb from "../../Components/Navbar/navbar";
-// import SmallCard from "../../Components/Card/card";
-
-// export default function Home() {
-//     return (
-//         <div>
-//             <Navb />
-//             <SmallCard iconPath="/icons/heart.png" contentTitle="Trust" contentText={
-//                 `We are experts in our \n sector , we are always here to help and advise you.`
-//             }
-//             />
-//             <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-//             <SmallCard iconPath="/icons/Icon-Support.png" contentTitle="Tradition and engagement" contentText={
-//                 `We are already the 5th generation taking care of our elders’ quality of life.`
-//             }
-//             />
-//         </div>
-//     );
-// }
