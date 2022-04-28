120cef61d2e3af838f646cb0deee87157ffe1d2d
Hatem Saadallah <saadallah.r.hatem@gmail.com>
2022-04-28 21:18:05 +0300

    edited header

--- src/Components/Header/header_part.component.js
+++ src/Components/Header/header_part.component.js
@@ -1,4 +1,4 @@
-import { Row, Col, Image } from 'antd';
+import { Row, Col, Image, Button } from 'antd';
 import 'antd/dist/antd.css';
 import './header_part.style.css';
 
@@ -8,11 +8,18 @@ export default function HeaderPart() {
             <Row>
                 {/* Add two equal Cols */}
                 <Col span={12}>
-
+                    <div className='div-welcome-text'>
+                        <h1 className='welcome-text'>At Naturlamb 1880 we are committed to improving your quality of life</h1>
+                        <p>therefore with our new range of Sports Orthopedics, we help you recover from injuries and regain mobility as quickly as possible</p>
+                        {/* change color */}
+                        <Button type='primary' className='button-round' >
+                            Contact Us
+                        </Button>
+                    </div>
                 </Col>
                 <Col span={12}>
                     <Image
-                    // Make width 100%
+                        // Make width 100%
                         style={{ width: '100%', height: '100%' }}
                         src="error"
                         className='header-image'
