4547e1f02a56643e09372043f17065be756a8fa6
Hatem Saadallah <saadallah.r.hatem@gmail.com>
2022-04-30 23:24:35 +0300

    SlideShow

--- src/Views/Products/products.view.js
+++ src/Views/Products/products.view.js
@@ -1,11 +1,11 @@
 import * as React from 'react';
 import Navb from "../../Components/Navbar/navbar";
-
+import Slideshow from "../../Components/Slideshow/slideshow.component";
 export default function Products() {
     return (
         <>
             <Navb />
-            
+            <Slideshow />
         </>
     );
 }
