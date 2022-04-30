09a6529bf43c3f42596b9e9e4c3ad9ed9468c2a6
Hatem R Saadallah <saadallah.r.hatem@gmail.com>
2022-04-30 23:58:52 +0300

    Added Slideshow & slide images

--- src/Views/Products/products.view.js
+++ src/Views/Products/products.view.js
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import Navb from "../../Components/Navbar/navbar";
 import Slideshow from "../../Components/Slideshow/slideshow.component";
+import ContactUsHome from '../../Components/HomePageSections/ContactUs/contactus.homepage.section';
 export default function Products() {
     return (
         <>
             <Navb />
             <Slideshow />
+            <ContactUsHome />
         </>
     );
 }
