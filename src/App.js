4d291dfcbb678ba28656b5872da93a40a05e0756
Mohammed Hijazi <mohammedhijazi199@gmail.com>
2022-04-30 04:44:17 +0300

    completet buttom navbar and fixes to top navbar

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./Views/Home/home.view";
 import About from "./Views/About/about.view";
 import Contact from "./Views/Contactus/contact.view";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "@fontsource/cairo";
 
 export default function App() {
   return (
