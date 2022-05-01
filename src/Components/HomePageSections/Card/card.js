aa86356138f8c81934622631ff495f37cf123a5f
Hatem R Saadallah <saadallah.r.hatem@gmail.com>
2022-05-01 22:51:14 +0300

    Working on Cards Shadow

--- src/Components/HomePageSections/Card/card.js
+++ src/Components/HomePageSections/Card/card.js
@@ -1,11 +1,10 @@
 import React from 'react';
-// import {Button} from "react-bootstrap";
 import Card from 'react-bootstrap/Card'
 import "./card.style.css";
 
 export default function SmallCard(props){
     return(
-        <Card style={{ width: (props.width ? props.width : "18em"),borderColor:"white", float:props.float, display: 'block', margin: props.margin}} className="cairo-font">
+        <Card style={{ width: (props.width ? props.width : "18em"),borderColor:"white", float:props.float, display: 'block', margin: props.margin}} className="cairo-font single-card-home">
             <Card.Img variant="top" src={props.iconPath} style={{width:85,height:85}} />
             <Card.Body>
                 <Card.Title >{props.contentTitle}</Card.Title>
