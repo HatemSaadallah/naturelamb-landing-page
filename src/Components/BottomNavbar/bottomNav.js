9774d3665710016d3e0d89da4f1aa1fa1c69540d
Hatem R Saadallah <saadallah.r.hatem@gmail.com>
2022-05-01 21:04:13 +0300

    Added Links to Nav Bar

--- src/Components/BottomNavbar/bottomNav.js
+++ src/Components/BottomNavbar/bottomNav.js
@@ -7,7 +7,7 @@ export default function BottomNav(){
             <Navbar className="Na" >
                     <img className="ima" src="/images/main.png" alt="logo"/>
                     <Nav className="N" >
-                        <Nav.Link  className="nLink"  href="#home">Home</Nav.Link>
+                        <Nav.Link  className="nLink"  href="/">Home</Nav.Link>
                         <Nav.Link className="nLink" href="#link">About us</Nav.Link>
                         <Nav.Link className="nLink"  href="#link">Contact us</Nav.Link>                            
                     </Nav>
--- src/Components/Navbar/navbar.js
+++ src/Components/Navbar/navbar.js
@@ -4,7 +4,7 @@ import React from 'react';
 export default function Navb(props){
     return(
             <Navbar className="Nb">
-                    <Navbar.Brand href="#home" style={{marginLeft:'85px'}}>
+                    <Navbar.Brand href="/" style={{marginLeft:'85px'}}>
                         <img
                             src={props.imagePath}
                             width="142.38"
@@ -16,10 +16,10 @@ export default function Navb(props){
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
                         <Nav className="nava" >
-                            <Nav.Link style={{paddingLeft:20,paddingRight:20}} href="#home" className="nlink">Home</Nav.Link>
-                            <Nav.Link style={{paddingLeft:20,paddingRight:20}} href="#link" className="nlink">Product</Nav.Link>
-                            <Nav.Link style={{paddingLeft:20,paddingRight:20}} href="#link" className="nlink" >About us</Nav.Link>
-                            <Nav.Link style={{backgroundColor:"darkgreen",borderRadius:35,paddingLeft:20,paddingRight:20,color:"white"}} href="#link">Contact us</Nav.Link>
+                            <Nav.Link style={{paddingLeft:20,paddingRight:20}} href="/" className="nlink">Home</Nav.Link>
+                            <Nav.Link style={{paddingLeft:20,paddingRight:20}} href="/products" className="nlink">Product</Nav.Link>
+                            <Nav.Link style={{paddingLeft:20,paddingRight:20}} href="/about" className="nlink" >About us</Nav.Link>
+                            <Nav.Link style={{backgroundColor:"darkgreen",borderRadius:35,paddingLeft:20,paddingRight:20,color:"white"}} href="/contact">Contact us</Nav.Link>
                             <Nav.Link style={{paddingLeft:80,paddingRight:20}} href="#link">(929)953-8315</Nav.Link>
                             <NavDropdown title="EN" id="basic-nav-dropdown" style={{color:'white'}}>
                                 <NavDropdown.Item href="#action/3.1">RU</NavDropdown.Item>
