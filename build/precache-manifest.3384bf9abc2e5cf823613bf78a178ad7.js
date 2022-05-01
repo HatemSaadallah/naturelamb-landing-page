9ca5bdcd4858d264e4684874f51f0a052bca6bf9
Hatem R Saadallah <saadallah.r.hatem@gmail.com>
2022-05-01 21:46:18 +0300

    Pushing Build Directory

---
+++ build/precache-manifest.3384bf9abc2e5cf823613bf78a178ad7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6dc55256d5d4fd38e3e1",
+    "url": "/static/css/main.5ab815c3.chunk.css"
+  },
+  {
+    "revision": "6dc55256d5d4fd38e3e1",
+    "url": "/static/js/main.0942f237.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "935319744411d4d722b6",
+    "url": "/static/css/2.e95987f0.chunk.css"
+  },
+  {
+    "revision": "935319744411d4d722b6",
+    "url": "/static/js/2.e1923cda.chunk.js"
+  },
+  {
+    "revision": "db122c7140cbed2083dbaa699eec35b2",
+    "url": "/index.html"
+  }
+];
