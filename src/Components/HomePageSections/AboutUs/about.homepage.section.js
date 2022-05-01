838474b22a01b83ec4d12c91341e19b90327cfee
Hatem R Saadallah <saadallah.r.hatem@gmail.com>
2022-05-01 22:05:32 +0300

    Fixed styling in about us section in homepage

--- src/Components/HomePageSections/AboutUs/about.homepage.section.js
+++ src/Components/HomePageSections/AboutUs/about.homepage.section.js
@@ -6,10 +6,10 @@ import './about.homepage.style.css';
 export default function AboutUsHome() {
     return (
         <>
-            <Row>
+            <Row className="about-home-section">
                 <Col span={11}>
-                    <p>About us</p>
-                    <h2>Naturlamb 1880 (history)</h2>
+                    <p className="about-us-home-word">About us</p>
+                    <h2 className="about-us-home-title">Naturlamb 1880 (history)</h2>
                     <p className='info-sec'>
                         García 1880, as the trademark of Sucesores de Manuel García,
                         S.L. is a family business created in 1880. It was born as a
@@ -19,11 +19,11 @@ export default function AboutUsHome() {
                     <p className='info-sec'>
                         In 1945, Manuel García Martínez Jr. joined the management of the company. His work throughout these post-war years was vital for the subsequent development of the company. He searched for new work formulas and began to mechanize the manufacturing process. In 1980 his children continued to manage the business.
                     </p>
-                    <Button variant="success">{'About Us>'} </Button>{' '}
+                    <Button className="about-us-home-button" variant="success">{'About Us>'} </Button>{' '}
 
                 </Col>
                 <Col span={13}>
-
+                    <img className="about-bg-image" src="/images/home/Path12.png" />
                 </Col>
             </Row>
         </>
